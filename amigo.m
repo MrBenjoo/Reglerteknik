50fd47a6dc4ac603ae2160a56e99b45663d0afa8
Unknown <benjo_953@hotmail.com>
2018-03-10 16:42:39 +0100

    amigo implementerad

--- amigo.m
+++ amigo.m
@@ -1,7 +1,10 @@
-function [K, TI, TD] = amigo(file)
+load('LambdaMetodenLTKs.mat')
 
 K = (1/Ks)*(0.2+0.45*T/L);
 
+Ti = ((0.4 * L + 0.8 * T) / ( L + 0.1 * T )) * L;
 
-end
+Td = ( (0.5 * L*T) / (0.3 * L + T));
+
+save('AmigoMetodenKTiTd.mat', 'K', 'Ti', 'Td');
 
